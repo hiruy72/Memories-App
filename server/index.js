96665f31fc71a436225be1d978d82eb8c8337544
Hiruy Legesse <hiruyadane@gmail.com>
2025-09-25 03:29:50 -0700

    connecting database

--- server/index.js
+++ server/index.js
@@ -3,3 +3,11 @@ import bodyParser from 'bodyParser'
 import mongoose from 'mongoose'
 import core from 'cors';
 
+const app = express();
+app.use(bodyParser.json({limit:"30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+app.use(cors());
+
+// mongo db 
+const CONNECTION_URL = 'mongodb+srv://admin:
+
