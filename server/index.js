bcf2f62246c8ebf9c6c07b21227130f6a5a985c8
Hiruy Legesse <hiruyadane@gmail.com>
2025-09-25 04:08:09 -0700

    backend Updates

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
-import express from 'express'
-import bodyParser from 'bodyParser'
-import mongoose from 'mongoose'
-import core from 'cors';
+import express from 'express';
+import bodyParser from 'body-parser'; // MUST be exactly this
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postsRoutes from './routes/posts.js'; // Importing the posts routes
 
 const app = express();
-app.use(bodyParser.json({limit:"30mb", extended:true}));
-app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+
+app.use('/posts', postsRoutes); // Using the posts routes
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-// mongo db 
-const CONNECTION_URL = 'mongodb+srv://admin:
+// MongoDB connection
+const CONNECTION_URL = 'mongodb+srv://hiruy:january62005@cluster0.elsuyko.mongodb.net/';
+const PORT = process.env.PORT || 5000;
 
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
